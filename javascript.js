dcccb283ee3842f62c7a7a5c04b46b31dbca3aa7
JehovasNES <thomas.jfry.vanallen@gmail.com>
2022-04-26 00:58:02 -0500

    container to global variable

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
-function generateGrid(totalCells) {
+const container = document.querySelector('#etch');
 
-    const container = document.querySelector('#etch');
+
+//working function, leave alone, unless changes are necessary, document on commit when changes are done
+function generateGrid(totalCells) {
 
     for(let gen = 0; gen < totalCells**2; gen++) {
         let grid = document.createElement('div')
@@ -12,11 +14,11 @@ function generateGrid(totalCells) {
 
 //function exponentiates by 2 to get real total(eg. 16^2)
 totalCells = 16;
-
 generateGrid(totalCells);
 
 
 //change squares to white for etching and sketching
+//this function will no longer work after the grid has been changes, not sure why
 const hoverBlack = document.querySelectorAll('.griddy');
 hoverBlack.forEach(function blackCube(hoverBlack) {
     hoverBlack.addEventListener('mouseover', function() {
@@ -28,10 +30,12 @@ hoverBlack.forEach(function blackCube(hoverBlack) {
 const gridChangeBtn = document.querySelector('.grid-gen');
 gridChangeBtn.addEventListener('click', function() {
     const userGrid = Number(window.prompt('Enter a number, 1-100', ""));
-
     totalCells = userGrid;
     generateGrid(totalCells);
 })
 
 //function to clear grid, changes background color
 const gridClear = document.querySelector('.clear')
+
+//function for gridSize psuedo code in file live-planning.txt
+//function cellSize()
