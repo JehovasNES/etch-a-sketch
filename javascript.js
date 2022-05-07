0d697617fa5979693b8ac9e2697172bcb4466259
JehovasNES <thomas.jfry.vanallen@gmail.com>
2022-05-07 00:25:45 -0500

    fixing bug and reorged

--- javascript.js
+++ javascript.js
@@ -10,29 +10,6 @@ function cellSize(container, totalCells) {
     document.querySelectorAll('.griddy').forEach(function(e) {e.style.width = cellWidth + 'px';})
 };
 
-
-function generateGrid(totalCells) {
-    for(let gen = 0; gen < totalCells**2; gen++) {
-        let grid = document.createElement('div')
-        grid.classList.add('griddy');
-        container.appendChild(grid);
-        cellSize(container, totalCells);
-    }
-}
-
-//function exponentiates by 2 to get real total(eg. 16^2)
-totalCells = 16;
-generateGrid(totalCells);
-
-//add white class on hover
-const hoverBlack = document.querySelectorAll('.griddy');
-hoverBlack.forEach(function blackCube(hoverBlack) {
-   hoverBlack.addEventListener('mouseover', function() {
-    hoverBlack.classList.add('color-white');
-   })
-});
-
-//works in the jankiest way, it actually just adds more grids to the container :/
 const gridChangeBtn = document.querySelector('.grid-gen');
 gridChangeBtn.addEventListener('click', function() {
     const userGrid = Number(window.prompt('Enter a number, 1-100', ""));
@@ -50,3 +27,37 @@ function clearScreen() {
 //clear screen button
 const gridClearBtn = document.querySelector('.clear');
 gridClearBtn.addEventListener('click', () => clearScreen());
+
+function removeGrid() {
+    const remove = document.querySelector("#etch");
+    while (remove.firstChild) {
+        remove.removeChild(container.lastChild); 
+    }
+}
+
+function generateGrid(totalCells) {
+    removeGrid();
+    for(let gen = 0; gen < totalCells**2; gen++) {
+        let grid = document.createElement('div')
+        grid.classList.add('griddy');
+        container.appendChild(grid);
+        cellSize(container, totalCells);
+    }
+}
+
+//function exponentiates by 2 to get real total(eg. 16^2)
+totalCells = 16;
+generateGrid(totalCells);
+
+//add white class on hover
+const hoverBlack = document.querySelectorAll('.griddy');
+function onHover() {
+hoverBlack.forEach(function blackCube(hoverBlack) {
+   hoverBlack.addEventListener('mouseover', function() {
+    hoverBlack.classList.add('color-white');
+    onHover();
+   })
+});
+};
+
+//works in the jankiest way, it actually just adds more grids to the container :/
